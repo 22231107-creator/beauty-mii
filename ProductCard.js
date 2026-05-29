fda873b8cbe420178ff2a629f6964e93553425c7
22231107-creator <22231107@students.liu.edu.lb>
2026-05-29 16:32:36 +0300

    Add files via upload

---
+++ ProductCard.js
@@ -0,0 +1,11 @@
+function ProductCard({ name, price, image }) {
+  return (
+    <div className="product-card">
+      <img src={image} alt={name} />
+      <h3>{name}</h3>
+      <p>{price}</p>
+    </div>
+  );
+}
+
+export default ProductCard;
