8d63a6ee90fa1e034daf4f6ffd6f8e23b0feb263
22231107-creator <22231107@students.liu.edu.lb>
2026-05-29 16:32:11 +0300

    Add files via upload

---
+++ Hero.js
@@ -0,0 +1,12 @@
+function Hero() {
+  return (
+    <div className="hero">
+     <img src="https://images.unsplash.com/photo-1522335789203-aabd1fc54bc9?w=1200" alt="Beauty products" width="100%" />
+      <div className="hero-text">
+        <h2>Radiant Skin Starts Here</h2>
+      </div>
+    </div>
+  );
+}
+
+export default Hero;
